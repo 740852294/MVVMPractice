8efaee7ed5867529aa4fa63b2758e6baea7efc52
Zheng Yuanle <yuanlezheng@163.com>
2022-02-25 13:25:11 +0800

    新增module_sample模块

---
+++ app/src/main/java/com/owen/mvvm/RApp.java
@@ -0,0 +1,14 @@
+package com.owen.mvvm;
+
+import com.owen.library_common.base.BaseApp;
+
+/**
+ * 项目名:     MVVMPractice
+ * 包名       com.owen.mvvm
+ * 文件名:    RApp
+ * 创建者:    Zheng Yuanle
+ * 创建时间:  2022/2/25 13:17
+ * 描述:
+ */
+public class RApp extends BaseApp {
+}
